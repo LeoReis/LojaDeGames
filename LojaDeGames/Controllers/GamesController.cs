64e21b56f76591cf3044ac013f3bc7cdea9978e8
LeoReis <leonardo.reis@catolicasc.org.br>
2017-08-14 20:18:38 -0300

    Alteração no estrutura

---
+++ LojaDeGames/Controllers/GamesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaDeGames.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            var game = new GamesController() {};
+            return View();
+        }
+    }
+}
---
+++ LojaDeGames/Models/Jogo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaDeGames.Models
+{
+    public class Jogo
+    {
+        private int id;
+        private string nome;
+    }
+}
