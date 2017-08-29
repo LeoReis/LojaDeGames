1d945d854b4df90a8323e8dc8aca60eff04cba66
LeoReis <leonardo.reis@catolicasc.org.br>
2017-08-29 10:59:34 -0300

    Atividade 2

--- LojaDeGames/Controllers/GamesController.cs
+++ LojaDeGames/Controllers/GamesController.cs
@@ -9,11 +9,32 @@ namespace LojaDeGames.Controllers
 {
     public class GamesController : Controller
     {
-        // GET: Games
-        public ActionResult Random()
+        static List<Jogo> listaJogos = new List<Jogo>()
         {
-            var game = new Jogo() { Id = 5,Nome ="GTA V"};
-            return View(game);
+            new Jogo{Id = 1,Nome = "GTA V",Genero = "Ação",FaixaEtaria = "18+",Desenvolvedora =  "ROCKSTAR GAMES"},
+            new Jogo{Id = 2,Nome = "Red Dead Redemption",Genero = "Aventura",FaixaEtaria = "18+",Desenvolvedora =  "ROCKSTAR GAMES"},
+            new Jogo{Id = 3,Nome = "Monster Hunter",Genero = "Aventura",FaixaEtaria = "12+",Desenvolvedora =  "Capcom"},
+            new Jogo{Id = 4,Nome = "Street Fighter V",Genero = "Luta",FaixaEtaria = "12+",Desenvolvedora =  "Capcom"},
+            new Jogo{Id = 5,Nome = "Resident Evil IV",Genero = "Ação|Horror",FaixaEtaria = "16+",Desenvolvedora =  "Capcom"}
+        };
+
+
+        public ActionResult Index()
+        {
+            var gameIndexView = new GameIndexViewModel()
+            {
+                Jogos = listaJogos
+            };
+            return View(gameIndexView);
+        }
+
+        public ActionResult DetalhesJogos(int id)
+        {
+            if (id > listaJogos.Count) return HttpNotFound();
+
+            Jogo jogoDetalhe = listaJogos.Find(jogos => jogos.Id == id);
+
+            return View(jogoDetalhe);
         }
     }
 }
--- LojaDeGames/Models/Jogo.cs
+++ LojaDeGames/Models/Jogo.cs
@@ -9,5 +9,10 @@ namespace LojaDeGames.Models
     {
         public int Id { get; set; }
         public string Nome { get; set;}
+        public string Genero { get; set; }
+        public string FaixaEtaria { get; set; }
+        public string Desenvolvedora { get; set; }
+
+
     }
 }
